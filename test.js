99f4d2c946d7cbe34c310204d12856523ea5a84d
hoya <monggae0319@gmail.com>
2026-01-11 23:46:02 +0900

    huge update

--- test.js
+++
@@ -1,5 +0,0 @@
-import { earthquake, tsunami, volcano } from "./src/index.js";
-
-console.log(earthquake.EarthquakeWarningKR('31'))
-console.log(earthquake.AreaEpicenterKR(105))
-console.log(tsunami.TsunamiWarningKR(53))
